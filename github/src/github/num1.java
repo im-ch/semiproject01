49a28802c16089ea0421f3117739d84fe3281024
im-ch <ckdgml118@naver.com>
2017-03-20 20:48:21 +0900

    fdsfd

--- github/src/github/num1.java
+++ github/src/github/num1.java
@@ -4,8 +4,20 @@ public class num1 {
 	
 	public static void main(String[] args) {
 		
+<<<<<<< HEAD
+<<<<<<< HEAD
+		System.out.println("sdasdfasdf");
+		System.out.println("ººÀ½¹ä");
+		System.out.println("»ï¼±Â«»Í");
+		System.out.println("°í±¸¸¶");
+		System.out.println("»çÀÌ´Ù");
+=======
 		System.out.println("ihihihihihihih");
+>>>>>>> refs/heads/num1
 		
+=======
+	System.out.println("Çô´ÏÇã´Ï-°ÅºÏ¼±");
+>>>>>>> refs/heads/uijiokkl
 	}
 }
 	
