cab6b6f218c64395010d68e80ab1f978a03a145c
heehyun <gmlgus3372@gmail.com>
2017-03-20 21:01:13 +0900

    ㅇㅁㄴㅇㄹ

--- github/src/github/num2.java
+++ github/src/github/num2.java
@@ -2,13 +2,9 @@ public class num2 {
 
   public static void main(String[] args) {
 
-	  System.out.println("橫霞檜陛 睡腦朝 お諦檜蝶 じじ");
-	  System.out.println("該雖彊嬴 蕨雌 粒檜 腆 纔棲梱");
-	  System.out.println("璽�� 樹棲朝 餵縑 陛堅 談橫и棻...");
-	  System.out.println("葆牆葆牆");
-	  System.out.println("葆牆葆牆");
-	  System.out.println("憲懊鎬??六六六六");
-	  System.out.println("六六六六六六六六六六六六六六六六六六六六六六六六六六六六六六");
+	  System.out.println("憲懊棻檜喫菟嬴");
+	  
+	  
   }
 
 }
