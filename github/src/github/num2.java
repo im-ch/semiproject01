4438840c8910e047707322b890ed78a3b384e2a0
heehyun <gmlgus3372@gmail.com>
2017-03-20 21:03:44 +0900

    ㄴㅇㄹㄴㅇㄹ

--- github/src/github/num2.java
+++ github/src/github/num2.java
@@ -1,23 +1,7 @@
 public class num2 {
 
   public static void main(String[] args) {
-
-<<<<<<< HEAD
-	  System.out.println("어진이가 부르는 트와이스 티티");
-	  System.out.println("머지않아 예쁜 꽃이 될 테니까");
-	  System.out.println("창희 언니는 집에 가고 싶어한다...");
-	  System.out.println("마쟈마쟈");
-	  System.out.println("마쟈마쟈");
-<<<<<<< HEAD
-=======
-	  System.out.println("알았찌??ㅋㅋㅋㅋ");
-	  System.out.println("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ");
->>>>>>> refs/heads/hi
-=======
-	  System.out.println("알았다이놈들아");
-	  
-	  
->>>>>>> refs/heads/hi
+	  System.out.println("알았다");
   }
 
 }
