014f033880fc7bea404a343f87e94fd5f5d368a7
heehyun <gmlgus3372@gmail.com>
2017-03-20 20:28:05 +0900

    집에가고파

--- github/src/github/num1.java
+++ github/src/github/num1.java
@@ -3,29 +3,7 @@ package github;
 public class num1 {
 	
 	public static void main(String[] args) {
-		
-		System.out.println("metoo");
-		System.out.println("hjjjjhjdr6yfgh");
-		System.out.println("fgfgfglp;klksdfsdfsdfsdfsdf");
-		System.out.println("sdfsdfsdf");
-		System.out.println("sdfsdfs");
-<<<<<<< HEAD
-<<<<<<< HEAD
-		System.out.println("hello");
-=======
->>>>>>> refs/heads/master
-		System.out.println("hihi");
-		System.out.println("짜증나ㅜ");
-<<<<<<< HEAD
-		System.out.println("집에가고싶다.......");
-=======
-		System.out.println("밍밍 집가고 싶다");
->>>>>>> refs/heads/master
-=======
-		System.out.println("이것이다");
-		System.out.println("asfhdsuoptgr4lcvawo7vukifv67v6");
-		System.out.println("냠냠 오키포키 꺄꺄 오키도키요");
->>>>>>> refs/remotes/origin/num1
+		System.out.println("집에가자");
 	}
 
 }
