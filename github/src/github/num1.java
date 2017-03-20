4adddbbbd5d633fe781bc19871c06d5722568ebb
heehyun <gmlgus3372@gmail.com>
2017-03-20 20:19:50 +0900

    ㅋㅋ

--- github/src/github/num1.java
+++ github/src/github/num1.java
@@ -10,21 +10,12 @@ public class num1 {
 		System.out.println("sdfsdfsdf");
 		System.out.println("sdfsdfs");
 <<<<<<< HEAD
-<<<<<<< HEAD
 		System.out.println("hello");
 =======
 >>>>>>> refs/heads/master
-=======
-		System.out.println("이것이다");
-=======
->>>>>>> branch 'master' of https://github.com/im-ch/semiproject01.git
 		System.out.println("hihi");
-<<<<<<< HEAD
 		System.out.println("짜증나ㅜ");
-=======
->>>>>>> refs/remotes/origin/master
->>>>>>> branch 'master' of https://github.com/im-ch/semiproject01.git
-		
+		System.out.println("밍밍 집가고 싶다");
 	}
 
 }
