dd0f4735f27fb784b23dc25fddce622a9caef3d2
YUEOJIN <eojin27@naver.com>
2017-03-20 20:40:56 +0900

    ㄷㅇㅇ

--- github/src/github/num1.java
+++ github/src/github/num1.java
@@ -4,21 +4,7 @@ public class num1 {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("metoo");
-		System.out.println("hjjjjhjdr6yfgh");
-		System.out.println("fgfgfglp;klksdfsdfsdfsdfsdf");
-		System.out.println("sdfsdfsdf");
-		System.out.println("sdfsdfs");
-		System.out.println("이것이다");
-		System.out.println("hihi");
-
-		
-		
-		System.out.println("Sfsdfsdf");		
-		System.out.println("Sfsdfsdf");
-		System.out.println("ddd");
-		
-		System.out.println("물어보지마");
+	System.out.println("혀니허니-거북선");
 	}
 
 }
