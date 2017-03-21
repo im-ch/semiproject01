02ef08df46b7f9686331885dd5a9df4ccab81d50
YUEOJIN <eojin27@naver.com>
2017-03-21 10:17:54 +0900

    rrr

--- github/src/github/num1.java
+++ github/src/github/num1.java
@@ -4,12 +4,7 @@ public class num1 {
 	
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
-		System.out.println("sdasdfasdf");
-		
-=======
-	System.out.println("혀니허니-거북선");
->>>>>>> refs/heads/uijiokkl
+		System.out.println("tt");
 	}
 }
 	
