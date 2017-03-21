dce0cb237ccd14bb0d732e512d68d89e7ac8041f
YUEOJIN <eojin27@naver.com>
2017-03-21 09:39:00 +0900

    ㄴㅇㄹㄴㅇㄹ

---
+++ github/src/github/num2.java
@@ -0,0 +1,9 @@
+public class num2 {
+
+  public static void main(String[] args) {
+	  System.out.println("건들지마세요");
+	  System.out.println("시간아 가지마....");
+	  System.out.println("어진이가 좋아하는 치즈");
+  }
+
+}
